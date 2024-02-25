c96d644f7c10d2b4bdc3f9fc2a9b18edefc18b8f
Maat98 <159943867+Maat98@users.noreply.github.com>
2024-02-25 23:51:58 +0300

    Created using Colaboratory

---
+++ python_input.py
@@ -0,0 +1,64 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNONhjnkOEGHmeb7kM2BaAf",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Maat98/Mathew/blob/main/python_input.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "name = input(\"Please enter your name: \")\n",
+        "age = input(\"Please enter your age: \")\n",
+        "location = input(\"Please enter your location: \")\n",
+        "\n",
+        "print(\"Name:\", name)\n",
+        "print(\"Age:\", age)\n",
+        "print(\"Location:\", location)\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "979YrAipBsbI",
+        "outputId": "4884ab7c-1bc5-47e9-9ed0-f46f8b716712"
+      },
+      "execution_count": 13,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Please enter your name: Mathews Onyango\n",
+            "Please enter your age: 21\n",
+            "Please enter your location: Kisumu\n",
+            "Name: Mathews Onyango\n",
+            "Age: 21\n",
+            "Location: Kisumu\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
